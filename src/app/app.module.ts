49ee9afb54b948f5666aa5a4ba862a3e9a81069b
Rafaela Lima Nogueira <rn-15@hotmail.com>
2022-01-24 11:41:43 -0300

    Configurando projeto para pt-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,12 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public formAgendamento: FormGroup;
+  public dataPreenchida: Date;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  public ngOnInit(): void {
+    this.iniciarFormulario();
+  }
+
+  public cadastrarAgendamento(): void {
+    console.log(this.formAgendamento.value.data);
+  }
+
+  private iniciarFormulario(): void {
+    this.formAgendamento = this.formBuilder.group({
+      cliente: ['', Validators.required],
+      pet: [''],
+      servico: ['', Validators.required],
+      data: ['', Validators.required],
+      status: ['', Validators.required],
+      observacao: ['']
+    });
+  }
+
+
 
 }
